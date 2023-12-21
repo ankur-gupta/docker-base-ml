8745422ca9871868dcc21bbdd7c707ab34fe50ca
ankur-gupta <7110058+ankur-gupta@users.noreply.github.com>
2023-12-21 00:43:56 -0800

    Improving vf install function

--- vf-install-env.fish
+++ vf-install-env.fish
@@ -1,7 +1,16 @@
 #!/usr/bin/env fish
 
 # Fish lists are not zero-indexed
-vf new $argv[1]
+set VF_ENV_NAME $argv[1]
+set PATH_TO_REQUIREMENTS_FILE $argv[2]
+
+# Check if PATH_TO_PYTHON_EXECUTABLE is provided, set a default if not
+if test (count $argv) -ge 3
+    set PATH_TO_PYTHON_EXECUTABLE $argv[3]
+    vf new "$VF_ENV_NAME" -p "$PATH_TO_PYTHON_EXECUTABLE"
+else
+    vf new "$VF_ENV_NAME"
+end
 pip install --upgrade pip
-pip install --no-cache-dir -r "/home/$USER/$argv[1].requirements.txt"
+pip install --no-cache-dir -r "$PATH_TO_REQUIREMENTS_FILE"
 vf deactivate
